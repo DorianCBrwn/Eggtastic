e418d6a3e0f4742fd46583d578cacbc45d9ed398
Cody Henthorne <cody.zbi@gmail.com>
2016-04-07 22:32:12 -0400

    start detail activity and load desc as html

--- app/src/main/java/com/universalmastery/eggtastic/DetailActivity.java
+++ app/src/main/java/com/universalmastery/eggtastic/DetailActivity.java
@@ -3,18 +3,29 @@ package com.universalmastery.eggtastic;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.widget.TextView;
 
 /**
  * Created by D.Brown on 3/29/16.
  */
 public class DetailActivity  extends AppCompatActivity{
+
+    public static final String RECIPE_DESC = "recipe_desc";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipe_detail);
-        this.requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+//        this.requestWindowFeature(getWindow().FEATURE_NO_TITLE);
 
         Intent intent = getIntent();
+
+        TextView textView = (TextView) findViewById(R.id.details_directions);
+        textView.setText(Html.fromHtml(intent.getStringExtra(RECIPE_DESC)));
+
+
+
 
     }
 }
